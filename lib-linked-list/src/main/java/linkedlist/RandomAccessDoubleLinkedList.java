6b1d3b4e9f27128155a7fb06f5b4023bd571b51e
omilton <omilton@LAPT-BP-RDSW-OM.burlington.evertz.tv>
2022-01-18 21:02:50 +0000

    First check in

---
+++ lib-linked-list/src/main/java/linkedlist/RandomAccessDoubleLinkedList.java
@@ -0,0 +1,232 @@
+package linkedlist;
+
+import java.util.Collection;
+import java.util.ConcurrentModificationException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class RandomAccessDoubleLinkedList<E> implements Collection<E> {
+
+  private Node head;
+  private Node tail;
+  private Map<E, Node> randomAccessMap = new HashMap<>();
+
+  private class Node {
+    Node prev;
+    E item;
+    Node next;
+    Node(E item, Node prev) {
+      this.item = item;
+      this.prev = prev;
+    }
+  }
+
+  @Override
+  public boolean add(E e) {
+    if (head == null) {
+      head = new Node(e, null);
+      tail = head;
+    } else {
+      Node after = tail;
+      tail = new Node(e, tail);
+      if (after != null) {
+        after.next = tail;
+      }
+      tail.prev = after;
+    }
+    randomAccessMap.put(e, tail);
+    return true;
+  }
+
+  @Override
+  public Iterator<E> iterator() {
+    return new Iterator<E>() {
+      int checkSize = randomAccessMap.size();
+      int count = 0;
+      Node current = head;
+      @Override
+      public boolean hasNext() {
+        checkForComodification();
+        return count < checkSize;
+      }
+
+      @Override
+      public E next() {
+        checkForComodification();
+        E result = null;
+        if (current != null) {
+          result = current.item;
+          current = current.next;
+          count ++;
+        }
+        return result;
+      }
+
+      private void checkForComodification() {
+        if (randomAccessMap.size() != checkSize) {
+          throw new ConcurrentModificationException("List size has changed");
+        }
+      }
+    };
+
+
+  }
+
+  @Override
+  public int size() {
+    return randomAccessMap.size();
+  }
+
+  @Override
+  public boolean isEmpty() {
+    return randomAccessMap.isEmpty();
+  }
+
+  @Override
+  public boolean contains(Object o) {
+    return randomAccessMap.containsKey(o);
+  }
+
+  @Override
+  public Object[] toArray() {
+    Object[] result = new Object[randomAccessMap.size()];
+    int i = 0;
+    Node node = head;
+    while (node != null) {
+      result[i++] = node.item;
+      node = node.next;
+    }
+    return result;
+  }
+
+  @Override
+  @SuppressWarnings("unchecked")
+  public <T> T[] toArray(T[] a) {
+    if (a.length < randomAccessMap.size()) {
+      a = (T[])java.lang.reflect.Array.newInstance(
+          a.getClass().getComponentType(), randomAccessMap.size());
+    }
+    int i = 0;
+    Object[] result = a;
+    for (Node x = head; x != null; x = x.next) {
+      result[i++] = x.item;
+    }
+
+    if (a.length > randomAccessMap.size()) {
+      a[randomAccessMap.size()] = null;
+    }
+
+    return a;
+  }
+
+  @Override
+  public boolean remove(Object o) {
+    Node remove = randomAccessMap.remove(o);
+    if (remove != null) {
+      if (remove.prev != null) {
+        remove.prev.next = remove.next;
+      }
+      if (remove.next != null) {
+        remove.next.prev = remove.prev.next;
+      }
+      if (remove == head) {
+        head = remove.next;
+      }
+      if (remove == tail) {
+        tail = remove .prev;
+      }
+      return true;
+    }
+    return false;
+  }
+
+  @Override
+  public boolean containsAll(Collection<?> c) {
+    return randomAccessMap.keySet().containsAll(c);
+  }
+
+  @Override
+  public boolean addAll(Collection<? extends E> c) {
+    for (E e : c) {
+      add(e);
+    }
+    return true;
+  }
+
+  @Override
+  public boolean removeAll(Collection<?> c) {
+    for (Object e : c) {
+      remove(e);
+    }
+    return true;
+  }
+
+  @Override
+  public boolean retainAll(Collection<?> c) {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public void clear() {
+    randomAccessMap.clear();
+    head = null;
+    tail = null;
+  }
+
+  public E get(Object o) {
+    Node node = randomAccessMap.get(o);
+    if (node != null) {
+      return node.item;
+    }
+    return null;
+  }
+
+  public void insertAfter(E after, E item) {
+    Node afterNode = randomAccessMap.get(after);
+    if (afterNode != null) {
+      Node node = new Node(item, afterNode);
+      if (afterNode.next != null) {
+        node.next = afterNode.next;
+        node.next.prev = node;
+      }
+      afterNode.next = node;
+      randomAccessMap.put(item, node);
+    } else {
+      throw new IllegalArgumentException("Could not find list node for ["+after+"]");
+    }
+  }
+
+  public void put(E was, E item) {
+    Node itemNode = randomAccessMap.get(was);
+    if (itemNode != null) {
+      itemNode.item = item;
+    } else {
+      throw new IllegalArgumentException("Could not find list node for ["+was+"]");
+    }
+  }
+
+  @Override
+  public String toString() {
+    Node print = head;
+    StringBuilder sb = new StringBuilder("RandomAccessDoubleLinkedList: \n");
+    while (print != null) {
+      sb.append("Node: ");
+      sb.append(print.item);
+      sb.append(" Prev: ");
+      if (print.prev != null) {
+        sb.append(print.prev.item);
+      }
+      sb.append(" Next: ");
+      if (print.next != null) {
+        sb.append(print.next.item);
+      }
+      sb.append("\n");
+      print = print.next;
+    }
+    sb.append(size());
+    return sb.toString();
+  }
+
+}
---
+++ lib-linked-list/src/test/java/linkedlist/RandomAccessDoubleLinkedListTest.java
@@ -0,0 +1,72 @@
+package linkedlist;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.time.ZonedDateTime;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class RandomAccessDoubleLinkedListTest {
+
+  /**
+   * Demo of an immutable sequence cache using a custom implementation of a double linked list with random access.
+   *
+   * Note, the linked list it's self is mutable and therefore must still be protected with locks
+   */
+
+  //an immutable sequence. note, all params must be immutable or made so in the constructor
+  public record Event(String assetId, String eventType) {}
+
+  // an immutable sequence. note, all params must be immutable or made so in the constructor
+  public record Sequence(String id, ZonedDateTime startDateTime, List<Event> events) {
+    public Sequence {
+      events = Collections.unmodifiableList(events);
+    }
+  }
+
+  @Test
+  void immutablePlaylist() {
+    // create mutable list - this will still need protection using locks
+    RandomAccessDoubleLinkedList<Sequence> list = new RandomAccessDoubleLinkedList<>();
+
+    // create immutable sequences
+    Sequence one = new Sequence("1", ZonedDateTime.now(), Collections.singletonList(new Event("TEST-1", "program")));
+    Sequence two = new Sequence("2", ZonedDateTime.now(), Collections.singletonList(new Event("TEST-1", "program")));
+    Sequence three = new Sequence("3", ZonedDateTime.now(), Collections.singletonList(new Event("TEST-1", "program")));
+    list.add(one);
+    list.add(two);
+    list.insertAfter(one, three);
+
+    // check list size
+    assertEquals(3, list.size());
+
+    // check the linked list has ordered them correctly
+    Iterator<Sequence> itr = list.iterator();
+    assertEquals("1", itr.next().id());
+    assertEquals("3", itr.next().id());
+    assertEquals("2", itr.next().id());
+
+    assertThrows(UnsupportedOperationException.class, () -> one.events.add(new Event("JML","Commercial")));
+
+    // compilation errors since records are immutable
+    // one.id = "2";
+    // one.id() = "2";
+
+    // Update a node in the list with a new entry
+    Sequence four = new Sequence("4", ZonedDateTime.now(), Collections.singletonList(new Event("TEST-1", "program")));
+    list.put(one, four);
+
+    // check list size
+    assertEquals(3, list.size());
+
+    // check the linked list has ordered them correctly
+    itr = list.iterator();
+    assertEquals("4", itr.next().id());
+    assertEquals("3", itr.next().id());
+    assertEquals("2", itr.next().id());
+  }
+
+
+}
