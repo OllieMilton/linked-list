2fd58bd20e9aa07a7bc0e73826b5da7d138c3533
Ollie Milton <miltonollie@gmail.com>
2023-04-20 12:08:42 +0100

    Update

--- lib-linked-list/src/main/java/linkedlist/partitioned/PartitionIndexFactory.java
+++ lib-linked-list/src/main/java/linkedlist/partitioned/PartitionIndexFactory.java
@@ -2,5 +2,11 @@ package linkedlist.partitioned;
 
 public interface PartitionIndexFactory<I extends Comparable<? super I>> {
 
-  PartitionIndex<I> newPartitionIndex(int existingCount, I index);
+  /**
+   * Creates a new {@link PartitionIndex} for the given index. Partition count is supplied so that implementations can throw an exception if a maximum number of partitions has been reached.
+   * @param partitionCount the number of existing partitions.
+   * @param index the index to create the partition for.
+   * @return The newly created {@link PartitionIndex}
+   */
+  PartitionIndex<I> newPartitionIndex(int partitionCount, I index);
 }
